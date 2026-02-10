57be32148cea0efe57766129ef1e45d3636c4452
sfroning88 <skdf2012@gmail.com>
2026-02-10 13:02:55 -0600

    Some files didnt include

--- app/(components)/RotatingBaby.tsx
+++ app/(components)/RotatingBaby.tsx
@@ -4,20 +4,20 @@ import Image from "next/image";
 import { ROTATING_SPEED } from "@/app/constants";
 
 export function RotatingBaby() {
-    return (
-        <div className="flex items-center justify-center">
-            <div className="relative">
-                <Image
-                src="/images/BabySean.jpg"
-                alt="Baby Sean"
-                width={300}
-                height={300}
-                className="rounded-full animate-spin-slow"
-                style={{
-                    animationDuration: `${60 / ROTATING_SPEED}s`,
-                }}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center">
+      <div className="relative">
+        <Image
+          src="/images/BabySean.jpg"
+          alt="Baby Sean"
+          width={300}
+          height={300}
+          className="rounded-full animate-spin-slow"
+          style={{
+            animationDuration: `${60 / ROTATING_SPEED}s`,
+          }}
+        />
+      </div>
+    </div>
+  );
 }
